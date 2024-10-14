4aeec2c1ecc3af13e80adf1558100f56b6489e55
RutamBhagat <rutambhagat@gmail.com>
2024-10-14 08:00:49 -0700

    add urls and visithistory schema

---
+++ project-01/src/schema/url.ts
@@ -0,0 +1,47 @@
+import type { InferSelectModel } from 'drizzle-orm';
+import { relations } from 'drizzle-orm';
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { createSelectSchema } from 'drizzle-zod';
+import { z } from 'zod';
+import { visitHistory } from './visit-history';
+
+export const urls = pgTable('urls', {
+  id: uuid('id').notNull().primaryKey().defaultRandom(),
+  shortID: text('short_id').notNull(),
+  redirectURL: text('redirect_url').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export const urlsRelations = relations(urls, ({ many }) => ({
+  visitHistories: many(visitHistory),
+}));
+
+export const selectUrlSchema = createSelectSchema(urls, {
+  shortID: schema => schema.shortID.min(1),
+  redirectURL: schema => schema.redirectURL.url(),
+});
+
+export const addUrlSchema = z.object({
+  body: selectUrlSchema.pick({
+    shortID: true,
+    redirectURL: true,
+  }),
+});
+
+export const updateUrlSchema = z.object({
+  body: selectUrlSchema.pick({
+    shortID: true,
+    redirectURL: true,
+  }).partial(),
+});
+
+export const deleteUrlSchema = z.object({
+  body: selectUrlSchema.pick({
+    id: true,
+  }),
+});
+
+export type Url = InferSelectModel<typeof urls>;
+export type NewUrl = z.infer<typeof addUrlSchema>['body'];
+export type UpdateUrl = z.infer<typeof updateUrlSchema>['body'];
---
+++ project-01/src/schema/visit-history.ts
@@ -0,0 +1,38 @@
+import type { InferSelectModel } from 'drizzle-orm';
+import { relations } from 'drizzle-orm';
+import { pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { createSelectSchema } from 'drizzle-zod';
+import { z } from 'zod';
+import { urls } from './url';
+
+export const visitHistory = pgTable('visit_history', {
+  id: uuid('id').notNull().primaryKey().defaultRandom(),
+  urlId: uuid('url_id').notNull().references(() => urls.id, { onDelete: 'cascade' }),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+});
+
+export const visitHistoryRelations = relations(visitHistory, ({ one }) => ({
+  url: one(urls, {
+    fields: [visitHistory.urlId],
+    references: [urls.id],
+  }),
+}));
+
+export const selectVisitHistorySchema = createSelectSchema(visitHistory, {
+  urlId: schema => schema.urlId.uuid(),
+});
+
+export const addVisitHistorySchema = z.object({
+  body: selectVisitHistorySchema.pick({
+    urlId: true,
+  }),
+});
+
+export const deleteVisitHistorySchema = z.object({
+  body: selectVisitHistorySchema.pick({
+    id: true,
+  }),
+});
+
+export type VisitHistory = InferSelectModel<typeof visitHistory>;
+export type NewVisitHistory = z.infer<typeof addVisitHistorySchema>['body'];
